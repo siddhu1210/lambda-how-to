7c01af3bb3cbd65948dd6960a8751c716a3d83a3
Siddharth <92784293+siddhu1210@users.noreply.github.com>
2021-12-31 14:34:15 +0530

    initial vs

---
+++ main.py
@@ -0,0 +1,9 @@
+#How to use the lambda function
+"""
+The lambda function is small anonymous function that can have many arguments but only one expression.
+Here is an example.
+"""
+def m2(num):
+    x = lambda num:num*2
+    print(x)
+m2(3)
