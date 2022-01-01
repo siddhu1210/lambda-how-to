d30695bc6c3509dc1212d13c22fcfc2999e20a4e
Siddharth <92784293+siddhu1210@users.noreply.github.com>
2022-01-01 10:38:15 +0530

    initial vs

--- main.py
+++ main.py
@@ -3,7 +3,4 @@
 The lambda function is small anonymous function that can have many arguments but only one expression.
 Here is an example.
 """
-def m2(num):
-    x = lambda num:num*2
-    print(x)
-m2(3)
+def mydoubler
